ebc5334ddc8ceef00835de27a599698955001e1c
Andre Goncalves <77490296+PhonyZ0n3@users.noreply.github.com>
2023-11-17 16:04:33 +0000

    Icon testing and side bar hover effect

--- frontend/src/app/layouts/Sidemenu.tsx
+++ frontend/src/app/layouts/Sidemenu.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import Image from 'next/image'
+'use client';
+
+import React, { useState } from 'react'
+import { Button } from "@nextui-org/react";
+import { Image } from "@nextui-org/react";
 
 export default function Sidemenu() {
+
+    const [sideBarWidth, setSideBarWidth] = useState('5%');
+
+    const handleMouseEnter = () => { setSideBarWidth('10%') };
+
+    const handleMouseExit = () => { setSideBarWidth('5%') }
+
+    //const image = <Image src='/icon.png' width={30} height={30} alt='image' />
+
     return (
-        <aside className='h-screen w-32'>
-            <nav className='h-full flex flex-col border-r-1 shadow'>
-                <div className='p-4 pb-2 flex justify-between items-center'>
-                    <Image
-                        src="/icon.png"
-                        alt="Example Image"
-                        width={100}
-                        height={100}
-                        className='rounded-lg'
-                    />
+        <aside
+            className='h-screen transition-all duration-300 ease-in-out'
+            style={{ width: sideBarWidth }}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseExit}>
+            <nav className='h-full flex flex-col border-r-1 shadow justify-start items-center py-5 px-3 gap-y-6'>
+                <div className='flex justify-center items-center'>
+                    <Button color='primary' className='rounded-full'>
+                    </Button>
                 </div>
             </nav>
         </aside>
