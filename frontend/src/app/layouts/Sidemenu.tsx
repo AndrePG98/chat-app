5c8f1fe5ebb54ea4de532f9d8f09baf0a0589bca
Andre Goncalves <77490296+PhonyZ0n3@users.noreply.github.com>
2023-11-18 19:27:47 +0000

    idk

--- frontend/src/app/layouts/Sidemenu.tsx
+++ frontend/src/app/layouts/Sidemenu.tsx
@@ -11,7 +11,10 @@ export default function Sidemenu() {
     return (
         <aside className='h-screen transition-all duration-300 ease-in-out py-5 px-4 border-r border-gray-700 flex flex-col'>
             <nav className='flex justify-center items-center'>
-                <Button isIconOnly className={`rounded-full transform ${isHovered ? 'scale-125' : 'scale-100'} transition-all ease-in-out`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+                <Button
+                    isIconOnly
+                    className={`rounded-full transform ${isHovered ? 'scale-125' : 'scale-100'} transition-all ease-in-out`}
+                    onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
                     <Image
                         src="/user.svg"
                         alt="User"
@@ -19,6 +22,9 @@ export default function Sidemenu() {
                         height={15}
                     />
                 </Button>
+                <span className="material-symbols-outlined">
+                    manage_search
+                </span>
             </nav>
         </aside>
     );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,10 +2,8 @@ import Sidemenu from './layouts/Sidemenu'
 
 export default function Home() {
   return (
-	<main className="flex min-h-screen flex-col items-center justify-between p-24">
-		<div className="flex justify-center space-x-8">
-			<RegisterCard></RegisterCard>
-			<LoginCard></LoginCard>
+	<main className="flex min-h-screen flex-col justify-between">
+		<div className="flex space-x-8">
 			<Sidemenu></Sidemenu>
 	  	</div>
 	</main>
