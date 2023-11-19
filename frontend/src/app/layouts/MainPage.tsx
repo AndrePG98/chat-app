7258461faa6c798b282d66ee0143496a7fcc97b6
Andre Goncalves <77490296+PhonyZ0n3@users.noreply.github.com>
2023-11-19 00:02:26 +0000

    new create button , transfered the buttons from sidemenu to main page, changed so that we selected channel type inside modal

--- frontend/src/app/layouts/MainPage.tsx
+++ frontend/src/app/layouts/MainPage.tsx
@@ -1,13 +1,27 @@
 "use client"
 
 import React, { useState } from 'react';
-import { Divider, Button, Popover, PopoverTrigger, PopoverContent, Input, RadioGroup, Radio } from '@nextui-org/react';
+import { Divider, Button, Input, RadioGroup, Radio, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, User, useDisclosure } from '@nextui-org/react';
 import VoiceChannel from '../shared/components/VoiceChannel';
 import TextChannel from '../shared/components/TextChannel';
 
 export default function MainPage() {
+    const [channels, setChannels] = useState<React.ReactNode[]>([]);
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [channelName, setChannelName] = useState('');
+    const [channelType, setChannelType] = React.useState("voice");
 
-    const [channels, setChannels] = useState([])
+    function createChannel() {
+        if (channelName.trim() !== '') {
+            let channel : React.ReactNode = null;
+            if(channelType === 'voice'){
+                channel = <VoiceChannel name={channelName}/>
+            } else {
+                channel = <TextChannel name={channelName}/>
+            }
+            setChannels((prevChannels) => [...prevChannels, channel]);
+        }
+    }
 
     const addIcon = <span className="material-symbols-outlined">add</span>
 
@@ -16,26 +30,63 @@ export default function MainPage() {
             <div className='basis-52 shrink-0 grow-0 border-r flex flex-col items-stretch justify-start py-6 px-3'>
                 <div className='text-center mb-10'>Group Name</div>
                 <div className='flex flex-row mb-5'>
-                    <Popover showArrow={true} placement="bottom" offset={20}>
-                        <PopoverTrigger>
-                            <Button isIconOnly color="success" className='flex justify-center items-center'>
-                                {addIcon}
-                            </Button>
-                        </PopoverTrigger>
-                        <PopoverContent className='flex flex-col items-stretch justify-start gap-4 p-5'>
-                            <RadioGroup className=''>
-                                <Radio value="voice">Voice Channel</Radio>
-                                <Radio value="text">Text Channel</Radio>
-                            </RadioGroup>
-                            <Input type="channelName" label="Channel Name" placeholder="Enter new channel name" className='' />
-                            <Button color='success'>Add</Button>
-                        </PopoverContent>
-                    </Popover>
+                    <Button isIconOnly color="success" className='flex justify-center items-center' onPress={onOpen}>
+                        {addIcon}
+                    </Button>
+                    <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                        <ModalContent>
+                            {(onClose) => (
+                                <>
+                                    <ModalHeader
+                                        className="flex flex-col gap-1">
+                                        What's the channel type?
+                                    </ModalHeader>
+                                    <ModalBody>
+                                        <RadioGroup value={channelType} onValueChange={setChannelType}>
+                                            <Radio value="voice">Voice</Radio>
+                                            <Radio value="text">Text</Radio>
+                                        </RadioGroup>
+                                    </ModalBody>
+                                    <ModalHeader className="flex flex-col gap-1">
+                                        What should the channel be called?
+                                    </ModalHeader>
+                                    <ModalBody>
+                                        <Input
+                                            type="name"
+                                            label="Channel Name"
+                                            placeholder="Enter the desired channel name"
+                                            value={channelName}
+                                            onChange={(e) => setChannelName(e.target.value)}
+                                        />
+                                    </ModalBody>
+                                    <ModalFooter>
+                                        <Button
+                                            color="danger"
+                                            variant="light"
+                                            onPress={onClose}>
+                                            Close
+                                        </Button>
+                                        <Button
+                                            className='w-full'
+                                            color="success"
+                                            onClick={() => {
+                                                createChannel()
+                                                onClose();
+                                                setChannelName('');
+                                            }}
+                                            endContent={<span className="material-symbols-outlined">add</span>}>
+                                            Create channel
+                                        </Button>
+                                    </ModalFooter>
+                                </>
+                            )}
+                        </ModalContent>
+                    </Modal>
                 </div>
                 <div className='channels-list flex flex-col gap-3'>
-                    <VoiceChannel name={"VoiceChannel 2"}></VoiceChannel>
-                    <VoiceChannel name={"VoiceChannel 2"}></VoiceChannel>
-                    <TextChannel name={"TextChannel 1"}></TextChannel>
+                    {channels.map((channel, index) => (
+                        <div key={index}>{channel}</div>
+                    ))}
                 </div>
             </div>
             <Divider className='w-1' orientation='vertical'></Divider>
--- frontend/src/app/layouts/Sidemenu.tsx
+++ frontend/src/app/layouts/Sidemenu.tsx
@@ -1,26 +1,11 @@
-'use client';
-
-import { Button, Divider, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, User, useDisclosure } from '@nextui-org/react';
-import React, { useState } from 'react';
+import { Divider,User} from '@nextui-org/react';
+import React from 'react';
 
 export default function Sidemenu() {
-    const [buttons, setButtons] = useState<React.ReactNode[]>([]);
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
-    const [channelName, setChannelName] = useState('');
 
-    function createButton() {
-        if (channelName.trim() !== '') {
-            const newButton = (
-                <Button key={channelName} className='w-full' endContent={<span className="material-symbols-outlined ml-8">edit</span>}>
-                    {channelName}
-                </Button>
-            );
-            setButtons((prevButtons) => [...prevButtons, newButton]);
-        }
-    }
 
     return (
-        <aside className='h-screen transition-all duration-300 ease-in-out py-5 px-4 border-r border-gray-700 flex flex-col absolute inset-y-0 left-0 w-60'>
+        <aside className='h-screen w-60 py-5 px-4 border-r border-gray-700 flex flex-col'>
             <nav className='flex justify-center items-center'>
                 <div className='space-y-3'>
                     <User
@@ -31,8 +16,6 @@ export default function Sidemenu() {
                         }}
                     />
                     <Divider className='w-52'></Divider>
-                    <div className="flex justify-center flex-col space-y-3">
-                    </div>
                 </div>
             </nav>
         </aside>
