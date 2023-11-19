276de0850db98785877ebd1f3399b6392e5e36c0
Andre Goncalves <77490296+PhonyZ0n3@users.noreply.github.com>
2023-11-19 13:02:40 +0000

    initial user channel list

--- frontend/src/app/shared/components/VoiceChannel.tsx
+++ frontend/src/app/shared/components/VoiceChannel.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import { Button } from '@nextui-org/react';
+'use client'
 
-export default function(props: {name : String}) {
+import React, { useState } from 'react';
+import { Button, Listbox, ListboxSection, ListboxItem, User, baseStyles } from '@nextui-org/react';
+
+export default function (props: { name: String }) {
+
+    const [channelUsers, setChannelUsers] = useState<React.ReactNode[]>([])
+
+    function addChannelUser() {
+        let newUser = <User
+            name="Jane Doe"
+            avatarProps={{
+                src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+            }}
+        />
+        setChannelUsers((prevChannelUsers) => [...prevChannelUsers, newUser]);
+    }
+
     return (
-        <Button className="h-8 w-full" endContent={<span className="material-symbols-outlined ml-8">volume_up</span>}>
-            {props.name}
-        </Button>
+        <div>
+            <Button className="h-8 w-full" endContent={<span className="material-symbols-outlined ml-8">volume_up</span>} onClick={addChannelUser}>
+                {props.name}
+            </Button>
+            <Listbox itemClasses={{
+                base : "ml-5"
+            }}>
+                {channelUsers.map((user, index) => (
+                    <ListboxItem key={index}>{user}</ListboxItem>
+                ))}
+            </Listbox>
+        </div>
     );
 }
