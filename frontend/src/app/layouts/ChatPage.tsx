4e945dd912a5dada0bf1179ea876ac5a9df0f5f5
Andre Goncalves <77490296+PhonyZ0n3@users.noreply.github.com>
2023-11-21 00:34:06 +0000

    new message input component and new message component

--- frontend/src/app/layouts/ChatPage.tsx
+++ frontend/src/app/layouts/ChatPage.tsx
@@ -1,23 +1,28 @@
 
 
 import React from 'react'
-import { Button, Input } from '@nextui-org/react'
+import { Button, Input, Card, Avatar } from '@nextui-org/react'
+import Message from '../shared/components/Message'
 
 export default function ChatPage() {
 
 
     return (
-        <div className='flex flex-col justify-between h-screen items-center'>
-            <div className='message-container overflow-y-auto flex-1 w-full'></div>
-            <Input className='' radius='none' placeholder='Say something...' size='lg' endContent={
-                <Button isIconOnly className='bg-transparent' disableRipple>
-                    <span className="material-symbols-outlined">
-                        send
-                    </span>
-                </Button>
-            }>
-
-            </Input>
+        <div className='flex flex-col h-screen'>
+            <div className='message-container overflow-y-auto flex-1 w-full pt-5 px-3 flex flex-col gap-3'>
+                <Message></Message>
+                <Message></Message>
+            </div>
+            <div className='mx-5 mb-4 mt-1'>
+                <Input className='' radius='md' variant='bordered' placeholder='Say something...' endContent={
+                    <Button isIconOnly className='bg-transparent' color='primary' disableRipple variant='flat'>
+                        <span className="material-symbols-outlined">
+                            send
+                        </span>
+                    </Button>
+                }>
+                </Input>
+            </div>
         </div>
     )
 }
