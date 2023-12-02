86f7849086634c551e18ab0835f747ba980aadc5
Andre Goncalves <77490296+PhonyZ0n3@users.noreply.github.com>
2023-12-02 04:09:39 +0000

    websocket testing comp, connect and disconnect working

--- backend/main.go
+++ backend/main.go
@@ -5,27 +5,69 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"log"
 	"net/http"
-	"time"
+
+	"github.com/gorilla/websocket"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+type Server struct {
+	clients map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		clients: make(map[*websocket.Conn]bool),
+	}
+}
+
+var server = NewServer()
+
+func ServeWS(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Error after Upgrade :", err.Error())
+		return
+	}
+
+	fmt.Printf("Estabilished Connection with: %s\n", conn.RemoteAddr())
+
+	server.clients[conn] = true
+
+	for {
+		msgType, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Error Reading:", err.Error())
+			return
+		}
+		fmt.Printf("%s sended : %s\n", conn.RemoteAddr(), string(msg))
+
+		//fmt.Println("Broadcasting message!")
+		fmt.Println("Number of clients:", len(server.clients))
+		for clientAddr := range server.clients {
+			if err := clientAddr.WriteMessage(msgType, msg); err != nil {
+				log.Println("Error broadcasting:", err)
+				return
+			}
+		}
+	}
+}
 
 func main() {
-	flag.Parse()
-	hub := newHub()
-	go hub.run()
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(hub, w, r)
-	})
-	server := &http.Server{
-		Addr:              *addr,
-		ReadHeaderTimeout: 3 * time.Second,
-	}
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+
+	http.HandleFunc("/ws", ServeWS)
+
+	fmt.Println("Server running on :", 8080)
+
+	http.ListenAndServe(":8080", nil)
+
 }
