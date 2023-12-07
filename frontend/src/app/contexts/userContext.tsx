4d48eb579293e9ac3acf89a8a3bcddf88d0756e3
Andre Goncalves <77490296+PhonyZ0n3@users.noreply.github.com>
2023-12-07 21:29:40 +0000

    minor changes

--- frontend/src/app/contexts/userContext.tsx
+++ frontend/src/app/contexts/userContext.tsx
@@ -1,34 +1,31 @@
-import React, { createContext, useContext } from 'react';
-import useWebSocket, { WebSocketData } from '../services/WebSocketService';
-import { User } from '../DTOs/User';
-import useConnectService from '../services/connectService';
+import React, { createContext, useContext } from "react"
+import useWebSocket, { WebSocketData } from "../services/WebSocketService"
+import { User } from "../DTOs/User"
+import useConnectService from "../services/connectService"
 
 interface userContextProps {
-    loggedUser: User
-    login: (userId: string, userName: string) => void,
-    sendWebSocketMessage: (data: any) => void;
-    receivedMessage: WebSocketData;
+	loggedUser: User
+	login: (userId: string, userName: string) => void
+	sendWebSocketMessage: (data: any) => void
+	receivedMessage: WebSocketData
 }
 
-
-const userContext = createContext<userContextProps | any>(undefined);
+const userContext = createContext<userContextProps | any>(undefined)
 
 export const useUserContext = () => {
-    const ctx = useContext(userContext) as userContextProps
-    if (!ctx) {
-        throw new Error("Bad contex")
-    }
-    return ctx
+	const ctx = useContext(userContext) as userContextProps
+	if (!ctx) {
+		throw new Error("Bad contex")
+	}
+	return ctx
 }
 
-
 export default function UserContextProvider(props: { children: React.ReactNode }) {
+	const { login, sendWebSocketMessage, receivedMessage, loggedUser } = useConnectService()
 
-    const { login, sendWebSocketMessage, receivedMessage, loggedUser } = useConnectService()
-
-    return (
-        <userContext.Provider value={{ login, sendWebSocketMessage, receivedMessage, loggedUser }}>
-            {props.children}
-        </userContext.Provider>
-    )
+	return (
+		<userContext.Provider value={{ login, sendWebSocketMessage, receivedMessage, loggedUser }}>
+			{props.children}
+		</userContext.Provider>
+	)
 }
